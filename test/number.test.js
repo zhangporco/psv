8fc0aff2fba8efbbe3ab75e21560ea99b31d3235
Porco home <zhangporco@gmail.com>
2017-08-14 14:56:45 +0800

    2.1.8 enum

--- test/number.test.js
+++ test/number.test.js
@@ -3,117 +3,117 @@
 var test = require('ava');
 var Psv = require('../index.js');
 
-// test('number 正确验证', function(t) {
-//     const schema = {
-//         key1: {
-//             type: Number,
-//             max: 5,
-//             min: 3,
-//             required: true
-//         }
-//     };
-//     const data = {
-//         key1: 3
-//     };
-//     const psv = new Psv(schema, data);
-//     const validate = psv.validate();
-//     t.true(validate)
-// });
-//
-// test('number 错误验证.非 number', function(t) {
-//     const schema = {
-//         key1: {
-//             type: Number,
-//             required: true
-//         }
-//     };
-//     const data = {
-//         key1: '1'
-//     };
-//     const psv = new Psv(schema, data);
-//     const validate = psv.validate();
-//     t.false(validate)
-// });
-//
-// test('number 错误验证.小于最小值', function(t) {
-//     const schema = {
-//         key1: {
-//             type: Number,
-//             max: 5,
-//             min: 3,
-//             required: true
-//         }
-//     };
-//     const data = {
-//         key1: 1
-//     };
-//     const psv = new Psv(schema, data);
-//     const validate = psv.validate();
-//     t.false(validate)
-// });
-//
-// test('number 错误验证.大于最大值', function(t) {
-//     const schema = {
-//         key1: {
-//             type: Number,
-//             max: 5,
-//             min: 3,
-//             required: true
-//         }
-//     };
-//     const data = {
-//         key1: 6
-//     };
-//     const psv = new Psv(schema, data);
-//     const validate = psv.validate();
-//     t.false(validate)
-// });
-//
-// test('number 正确验证.嵌套', function(t) {
-//     const schema2 = {
-//         key3: {
-//             type: Number,
-//             required: true
-//         }
-//     };
-//     const schema = {
-//         key1: {
-//             type: schema2,
-//             required: true
-//         }
-//     };
-//     const data = {
-//         key1: {
-//             key3: 1
-//         }
-//     };
-//     const psv = new Psv(schema, data);
-//     const validate = psv.validate();
-//     t.true(validate)
-// });
-//
-// test('number 错误验证.嵌套', function(t) {
-//     const schema2 = {
-//         key3: {
-//             type: String,
-//             required: true
-//         }
-//     };
-//     const schema = {
-//         key1: {
-//             type: schema2,
-//             required: true
-//         }
-//     };
-//     const data = {
-//         key1: {
-//             key3: 1
-//         }
-//     };
-//     const psv = new Psv(schema, data);
-//     const validate = psv.validate();
-//     t.false(validate)
-// });
+test('number 正确验证', function(t) {
+    const schema = {
+        key1: {
+            type: Number,
+            max: 5,
+            min: 3,
+            required: true
+        }
+    };
+    const data = {
+        key1: 3
+    };
+    const psv = new Psv(schema, data);
+    const validate = psv.validate();
+    t.true(validate)
+});
+
+test('number 错误验证.非 number', function(t) {
+    const schema = {
+        key1: {
+            type: Number,
+            required: true
+        }
+    };
+    const data = {
+        key1: '1'
+    };
+    const psv = new Psv(schema, data);
+    const validate = psv.validate();
+    t.false(validate)
+});
+
+test('number 错误验证.小于最小值', function(t) {
+    const schema = {
+        key1: {
+            type: Number,
+            max: 5,
+            min: 3,
+            required: true
+        }
+    };
+    const data = {
+        key1: 1
+    };
+    const psv = new Psv(schema, data);
+    const validate = psv.validate();
+    t.false(validate)
+});
+
+test('number 错误验证.大于最大值', function(t) {
+    const schema = {
+        key1: {
+            type: Number,
+            max: 5,
+            min: 3,
+            required: true
+        }
+    };
+    const data = {
+        key1: 6
+    };
+    const psv = new Psv(schema, data);
+    const validate = psv.validate();
+    t.false(validate)
+});
+
+test('number 正确验证.嵌套', function(t) {
+    const schema2 = {
+        key3: {
+            type: Number,
+            required: true
+        }
+    };
+    const schema = {
+        key1: {
+            type: schema2,
+            required: true
+        }
+    };
+    const data = {
+        key1: {
+            key3: 1
+        }
+    };
+    const psv = new Psv(schema, data);
+    const validate = psv.validate();
+    t.true(validate)
+});
+
+test('number 错误验证.嵌套', function(t) {
+    const schema2 = {
+        key3: {
+            type: String,
+            required: true
+        }
+    };
+    const schema = {
+        key1: {
+            type: schema2,
+            required: true
+        }
+    };
+    const data = {
+        key1: {
+            key3: 1
+        }
+    };
+    const psv = new Psv(schema, data);
+    const validate = psv.validate();
+    t.false(validate)
+});
 
 test('number 正确验证.枚举', function(t) {
     const schema = {
